33db9944be0855f7d58c79e55b8f4008207deb37
CHOI SI-HUN <sihun.choi@email.rhya-network.kro.kr>
2023-11-18 18:28:01 +0900

    PIP 설치 파일 생성

---
+++ install_package.bat
@@ -0,0 +1,10 @@
+@echo off_
+
+pip install opencv-python
+
+pip install ultralytics
+
+pip uninstall torch
+pip uninstall torchvision
+
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 -f https://download.pytorch.org/whl/torch_stable.html
